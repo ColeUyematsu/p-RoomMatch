fdcff72e3c520b76e43e3deba5b03e7187510b49
ColeUyematsu <cjuyematsu@gmail.com>
2024-12-03 15:18:16 -0800

    website

--- website/views.py
+++ website/views.py
@@ -10,33 +10,7 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    # if request.method == 'POST':
-    #     note = request.form.get('note')  # Gets the note from the HTML
-    #
-    #     if len(note) < 1:
-    #         flash('Note is too short!', category='error')
-    #     else:
-    #         new_note = Note(data=note, user_id=current_user.id)  # Schema for the note
-    #         db.session.add(new_note)  # Add the note to the database
-    #         db.session.commit()
-    #         flash('Note added!', category='success')
-
     return render_template("home.html", user=current_user)
-
-
-# @views.route('/delete-note', methods=['POST'])
-# @login_required
-# def delete_note():
-#     note = json.loads(request.data)  # This function expects a JSON from the INDEX.js file
-#     noteId = note['noteId']
-#     note = Note.query.get(noteId)
-#     if note:
-#         if note.user_id == current_user.id:
-#             db.session.delete(note)
-#             db.session.commit()
-#
-#     return jsonify({})
-
 
 @views.route('/questionnaire', methods=['GET', 'POST'])
 @login_required
@@ -70,7 +44,6 @@ def questionnaire():
 
     return render_template("questionnaire.html", form=form, user=current_user)
 
-
 @views.route('/questionnaire/view')
 @login_required
 def view_questionnaire():
@@ -83,49 +56,7 @@ def view_questionnaire():
     response_data = {field: getattr(response, field) for field in QuestionnaireResponse.fields.keys()}
     return render_template('view_questionnaire.html', response=response_data, user=current_user)
 
-
-# @views.route('/questionnaire', methods=['GET', 'POST'])
-# @login_required
-# def questionnaire():
-#     form = QuestionnaireForm()
-#     # Check if the user already has a response
-#     existing_response = QuestionnaireResponse.query.filter_by(user_id=current_user.id).first()
-#
-#     if form.validate_on_submit():
-#         # Update if response exists, else create a new one
-#         if existing_response:
-#             existing_response.question1 = form.question1.data
-#             existing_response.question2 = form.question2.data
-#             existing_response.question3 = form.question3.data
-#             flash('Your questionnaire responses have been updated!', 'success')
-#         else:
-#             new_response = QuestionnaireResponse(
-#                 user_id=current_user.id,
-#                 question1=form.question1.data,
-#                 question2=form.question2.data,
-#                 question3=form.question3.data
-#             )
-#             db.session.add(new_response)
-#             flash('Thank you for completing the questionnaire!', 'success')
-#
-#         db.session.commit()
-#         return redirect(url_for('views.home'))  # Redirect to home or another page
-#
-#     # Pre-fill the form if there are existing responses
-#     if existing_response:
-#         form.question1.data = existing_response.question1
-#         form.question2.data = existing_response.question2
-#         form.question3.data = existing_response.question3
-#
-#     return render_template("questionnaire.html", form=form, user=current_user)
-#
-#
-# @views.route('/questionnaire/view')
-# @login_required
-# def view_questionnaire():
-#     response = QuestionnaireResponse.query.filter_by(user_id=current_user.id).first()
-#     if not response:
-#         flash('You have not completed the questionnaire yet.', 'warning')
-#         return redirect(url_for('views.questionnaire'))
-#     return render_template('view_questionnaire.html', response=response, user=current_user)
-#
+# New About Route
+@views.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
